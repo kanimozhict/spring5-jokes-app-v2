7ada17dfdf6b82264cb720fd030cfe133a42d522
kanimozhict <kanimozhict1571995@gmail.com>
2022-06-28 19:39:58 -0400

    created a service implementation class

---
+++ src/main/java/services/Jokeserviceimpl.java
@@ -0,0 +1,21 @@
+package services;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Jokeserviceimpl implements Jokeservices{
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public Jokeserviceimpl() {
+        this.chuckNorrisQuotes =new ChuckNorrisQuotes() ;
+    }
+
+
+    @Override
+    public String getjoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
---
+++ src/main/java/services/Jokeservices.java
@@ -0,0 +1,5 @@
+package services;
+
+public interface Jokeservices {
+    String getjoke();
+}
