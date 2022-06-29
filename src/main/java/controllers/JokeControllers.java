61a18abf8395e0272def3a079a1fb4a016e58d7d
kanimozhict <kanimozhict1571995@gmail.com>
2022-06-28 20:07:03 -0400

    thymeleaf and controllers done

---
+++ src/main/java/controllers/JokeControllers.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import services.Jokeservices;
+
+public class JokeControllers {
+    private final Jokeservices jokeservices;
+@Autowired
+    public JokeControllers(Jokeservices jokeservices) {
+        this.jokeservices = jokeservices;
+    }
+    @RequestMapping({"/ ",""})
+    public String showJoke(Model model){
+    model.addAttribute("joke",jokeservices.getjoke());
+    return "index";
+
+    }
+}
